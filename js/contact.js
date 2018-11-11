6c08a24b290001c7ba65d36036e486dbf7a6384d
Johan Cañari <johanc.cca@gmail.com>
2018-11-11 04:22:24 -0500

    ADD

--- js/contact.js
+++ js/contact.js
@@ -11,13 +11,61 @@ $(document).ready(function(){
     // validate contactForm form
     $(function() {
 
+        var users = [
+            {'user' : 'user1-123'},
+            {'user' : 'user2-123'},
+            {'user' : 'user3-123'},
+            {'user' : 'user4-123'}
+        ];
+
+        $("#btn_login").click(function(){
+            var user_val = $("#username").val();
+            var pass_Val = $("#password").val();
+            if(user_val !== '' && pass_Val !== ''){
+                var result = users.find( user => user.user === user_val+'-'+pass_Val);
+                if(result !== undefined){
+                    switch(user_val){
+                        case 'user1':
+                            window.location.href = 'prosegur_view_1.html';
+                            break;
+                        case 'user2':
+                            window.location.href = 'prosegur_view_2.html';
+                            break;
+                        case 'user3':
+                            window.location.href = 'prosegur_view_3.html';
+                            break;
+                        case 'user4':
+                            window.location.href = 'prosegur_view_4.html';
+                            break;
+                    }
+                }else{
+                    $("#modal_error_login").modal("show");
+                }
+            }else{
+                $("#modal_error_login").modal("show");
+            }
+        });
+
         $("#btn_view_1").click(function(){
             var v1_emp_trans = $("#v1_emp_trans").val();
             var v1_placa_tractor = $("#v1_placa_tractor").val();
             var v1_placa_plataforma = $("#v1_placa_plataforma").val();
             var v1_destino_proveedor = $("#v1_destino_proveedor").val();
             if(v1_emp_trans !== '' && v1_placa_tractor !== '' && v1_placa_plataforma !== '' && v1_destino_proveedor !==  ''){
+                document.getElementById("form_view_1").reset();
                 $("#modal_qr").modal("show");
+            }
+        });
+
+        $("#btn_view_2").click(function(){
+            var v2_productos = $("#v2_productos").val();
+            var v2_guia_remi = $("#v2_guia_remi").val();
+            var v2_guia_trans = $("#v2_guia_trans").val();
+            var v2_onu = $("#v2_onu").val();
+            var v2_clase_riesgo = $("#v2_clase_riesgo").val();
+            if(v2_productos !== '' && v2_guia_remi !== '' && v2_guia_trans !== '' && v2_onu !== '' && v2_clase_riesgo !== ''){
+                document.getElementById("form_view_2").reset();
+                window.location.href = 'login.html';
             }
         });
 
